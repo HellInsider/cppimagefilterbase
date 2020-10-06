edb6e3c8ae372437a5cb87d25c79a99cd9b0da81
meso_test <45484407+HellInsider@users.noreply.github.com>
2020-10-06 18:23:29 +0300

    Intense bug fixed

--- FilterBlackWhite.cpp
+++ FilterBlackWhite.cpp
@@ -5,16 +5,13 @@ void FilterBlackWhite::MakeAction(int Ut, int Lt, int Dt, int Rt, png_toolkit* s
 	InputDataProcess(Ut, Lt, Dt, Rt, studTool);
 
 	int i, j;
-	float x;
+	int x;
 
 	for (i = U; i < D; i++)
 	{
 		for (j = L; j < R; j += 1)
 		{
-			x = Image.pixels[(i*Image.w + j) * Image.compPerPixel] * 0.3 +
-				Image.pixels[(i*Image.w + j) * Image.compPerPixel + 1] * 0.6 +
-				Image.pixels[(i*Image.w + j) * Image.compPerPixel + 2] * 0.1;
-
+			x =  GetPixelIntense(Image, j, i);
 			SetPixel(Image, j, i, x, x, x);
 		}
 	}
